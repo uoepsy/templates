782b01dcff7666cca647c18cd8abe377aa55aadf
Umberto Noe <9914422+unoe@users.noreply.github.com>
2020-04-25 00:00:37 +0100

    Added optional box

--- template_labs/dropdowns.R
+++ template_labs/dropdowns.R
@@ -102,3 +102,58 @@ generate_idq2 <- function() {
     
     return(id)
 }
+
+
+
+# Optional
+
+optbegin<-function(label=NULL,slabel=TRUE,show=TRUE){
+    if(!is.null(label)){label = as.character(label)}
+    if(slabel){slab = "Optional "}else{slab = ""}
+    if(show){
+        if (knitr::is_html_output()) {
+            id <- generate_id2()
+            id1 <- paste0("opt-start-", id)
+            id2 <- paste0("opt-body-", id)
+            part1 <- paste0("<div class=\"optional-begin\">",slab,label,
+                            sprintf("<span id='%s' class=\"fa fa-plus optional-icon clickable\" onclick=\"toggle_visibility('%s', '%s')\"></span>",id1, id2, id1),
+                            "</div>",
+                            paste0("<div class=\"optional-body\" id = \"",id2, "\" style=\"display: none;\">")
+            )
+            output <- structure(part1, format = "HTML", class = "knitr_kable")
+        }
+        if(knitr::is_latex_output()){
+            id <- generate_id2()
+            part1 <- paste0("\\textbf{",slab,label,"}")
+            output <- part1
+        }
+    } else {
+        if (knitr::is_html_output()){
+            part1 <- paste0("<div style=\"display:none;\">")
+            output <- structure(part1, format = "HTML", class = "knitr_kable")
+        }
+        if(knitr::is_latex_output()){
+            id <- generate_id2()
+            part1 <- paste0("\\iffalse")
+            output <- part1
+        }
+    }
+    return(output)
+}
+
+optend<-function(show=TRUE){
+    if (knitr::is_html_output()) {
+        part2 <- "</div><p class=\"optional-end\"></p>"
+        output <- structure(part2, format = "HTML", class = "knitr_kable")
+    }
+    if(knitr::is_latex_output()){
+        if(show){
+            part2 <- ""
+            output <- part2
+        }else{
+            part2 <- paste0("\\fi")
+            output <- part2
+        }
+    }
+    return(output)
+}
