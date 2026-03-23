a7d4cdad5f9f354a3ec5cd5d15f7b84eeb89a58b
Josiah King <josiahpjking@gmail.com>
2026-03-23 11:32:08 +0000

    test formative style

---
+++ docs/2627/formatives/block1_formative.R
@@ -0,0 +1,24 @@
+
+
+# Skill C1: I can manage libraries/packages in R.
+## Week 1 Q 1: Load the tidyverse package  
+
+
+
+
+
+# Skill S2: I can compute appropriate measure(s) of dispersion for a given variable.
+## Week 3 Q 3: Calculate the mean of .... blah blah
+
+
+
+
+# Skill C4: I can extract specific subsets of a given dataset.
+## Week 4 Q 4: Find the names of the two goats in the seattlepets.csv dataset
+### (you'll need the pets data for this one, so here is the code to read it in:)
+spets <- read.csv("https://uoepsy.github.io/data/pets_seattle.csv")
+
+
+
+# Skill ...
+## Week X Q X: ....
